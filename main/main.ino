f1d4f6f0801420bacba75b792de180ff7cc2460c
Geon Yoon <ssy01013@gmail.com>
2019-12-08 16:50:10 -0500

    Increase the readability

--- main/main.ino
+++ main/main.ino
@@ -9,58 +9,62 @@ Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // charger info setup
   pinMode(PIN13, INPUT);
-  Serial.begin(9600);
 
-  // ring
+  // ring setup 
   ring.begin();
   ring.setBrightness(32);
   ring.clear(); // clear all pixels
   ring.setPixelColor(0,0);
   ringshow_noglitch(); 
+
+  // For debug
+  Serial.begin(9600);
 }
 
+// initial variables
 byte prev0 = 1;
 int r= 255;
 int g = 0;
 int b = 0;
 
+// Turn ON LED
+void turn_on(int num_leds){
+  int j = 1;
+  for(int i=0;i<num_leds;i++) {
+    if(i< 12){
+      ring.setPixelColor(i,r,g+(i*21),b); 
+    }
+    if(i >= 12){
+      ring.setPixelColor(i,r-(j*40),255,b); 
+      j++;    
+    }
+  }
+}
+
+// Turn OFF LED
+void trun_off(int num_leds){
+  for(int i= num_leds+1;i<16;i++) {
+     ring.setPixelColor(i,0,0,0); 
+  }
+}
 
 void loop() {
-  int num_leds = 16;
-  byte curr0 = digitalRead(PIN13); 
+  
+  int num_leds = 8;               // hard coded for now. In the future, this will be changed by the phone's battery percentage
+  byte curr0 = digitalRead(PIN13); // get the signal from the charger. 1 == ON, 0 == OFF
+  
   if(curr0 != prev0){ 
     if(curr0 == 1){
-      // previous state was 0 and when it becomes 1
       // when the phone starts to charge
-      int j = 1;
-      for(int i=0;i<num_leds;i++) {
-        if(i< 12){
-          ring.setPixelColor(i,r,g+(i*21),b); 
-        }
-        if(i >= 12){
-          ring.setPixelColor(i,r-(j*40),255,b); 
-          j++;    
-        }
-      }
-      
-       if(num_leds == 0){
-        ring.setPixelColor(0,0,0,0); 
-        }
-      
-       //Turn off any remaining LED's
-       for(int i= num_leds+1;i<16;i++) {
-           ring.setPixelColor(i,0,0,0); 
-      }
+      turn_on(num_leds);
+      trun_off(num_leds);
       ringshow_noglitch(); 
     }
     else{
-      Serial.println("Finish charging");
-      //Turn off entire LED's
-       for(int i= 0;i<16;i++) {
-           ring.setPixelColor(i,0,0,0); 
-      }
+      // when the phone leaves the charger
+      trun_off(-1);
       ringshow_noglitch();    
     }
    }
