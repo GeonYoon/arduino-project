b81a872b4de607e377e9d7da8dfd768d40f5c063
Geon Yoon <ssy01013@gmail.com>
2019-12-16 22:01:06 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,18 @@
-#include <Adafruit_NeoPixel.h>
 #include <WiFi.h> // header we need to include for WiFi functions
 #include <WebServer.h>
+#include <Adafruit_NeoPixel.h>
 
 #define BUTTON0 34
 #define PIN 25
 #define PIN13 13    // Grap Signal from the charger
-#define NUM_LEDS 16 // Number of LEDs on the neo pixel ring
+#define PIN15 15    // LED wire LED Data 
+#define NUM_LEDS 75 // Number of LEDs on the neo pixel ring
 #define ringshow_noglitch() {delay(1);ring.show();delay(1);ring.show();}
-Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(75, PIN15, NEO_GRB + NEO_KHZ800);
+
+const char* ssid = "gun_final"; // !!! CHANGE THIS to your name or sth. unique
+const char* pass = "cics290m";
+WebServer server(80);  // define server object, at port 80
 
 // initial variables
 byte prev0 = 1;
@@ -16,11 +21,6 @@ int g = 0;
 int b = 0;
 int led = 0;
 
-// local wifi
-const char* ssid = "geonyoon"; 
-const char* pass = "pass";
-WebServer server(80); 
-
 // functions go here 
 void on_set() {
   if(server.hasArg("value")) {
@@ -28,7 +28,7 @@ void on_set() {
     int led_value = server.arg("value").toInt();
 
     // convert the battery to the proper number of LED
-    float temp = 16 * (led_value/100.0);
+    float temp = 75 * (led_value/100.0);
     led = int(temp);
 
     // send response back to the pohone
@@ -52,16 +52,16 @@ void turn_on(int num_leds){
 
 // Turn OFF LED
 void trun_off(int num_leds){
-  for(int i= num_leds+1;i<16;i++) {
+  for(int i= num_leds+1;i<75;i++) {
      ring.setPixelColor(i,0,0,0); 
   }
 }
 
-
-void setup() {
-  
+void setup(void){
   // charger info setup
   pinMode(PIN13, INPUT);
+  // get info
+  pinMode(PIN15, OUTPUT);
 
   // ring setup 
   ring.begin();
@@ -72,23 +72,19 @@ void setup() {
 
   // For debug
   Serial.begin(9600);
-
-  // wifi setup
-  WiFi.mode(WIFI_AP); // set ESP in AP mode
-  WiFi.softAP(ssid, pass); // sets ssid and password
+  
+  WiFi.mode(WIFI_AP); // start ESP in AP mode
+  WiFi.softAP(ssid, pass); // configure ssid and (optionally) password 
   server.on("/set", on_set); // set callback function
-  server.begin();
+  server.begin();  // starts server
 }
 
-void loop() {
-  
+void loop(void){
   int num_leds = led;              // based on the phone's battery
   byte curr0 = digitalRead(PIN13); // get the signal from the charger. 1 == ON, 0 == OFF
-
-  // run server
-  server.handleClient();
   
-  if(curr0 != prev0){ 
+  server.handleClient();  // handle client requests, must call frequently
+   if(curr0 != prev0){ 
     if(curr0 == 1){
       // when the phone starts to charge
       turn_on(num_leds);
@@ -98,7 +94,8 @@ void loop() {
     else{
       // when the phone leaves the charger
       trun_off(-1);
-      ringshow_noglitch();    
+      ringshow_noglitch();
+      num_leds = 0;    
     }
    }
    prev0 = curr0;
